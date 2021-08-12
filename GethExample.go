d8480ac9e1d01cb89f63e5ea415081ffeef55e0b
Justin Tempelman <jtempelm@gmail.com>
2021-08-11 21:03:57 -0500

    - transfer example

--- GethExample.go
+++ GethExample.go
@@ -15,7 +15,7 @@ import (
 	"os"
 )
 
-const sourceWalletAddress = "0x26006236eaB6409D9FDECb16ed841033d6B4A6bC"                          //first account from ganache-cli -m "test"
+const sourceWalletAddress = "0x26006236eaB6409D9FDECb16ed841033d6B4A6bC"  //first account from ganache-cli -m "test"
 var sourcePrivateKeyString = os.Getenv("CRYPTO_TEST_ACCOUNT_PRIVATE_KEY") //we get this from that same ganache-cli output
 
 func main() {
@@ -30,6 +30,7 @@ func main() {
 	client, err := ethclient.Dial(blockchainServer)
 	panicIfError(err)
 
+	fmt.Printf("==Source Address==\n")
 	logBalancesForAddress(client, destinationAccount.Address)
 
 	//collect the info we need to transfer an eth balance into it
@@ -45,36 +46,26 @@ func main() {
 
 	fmt.Printf("Suggested Gas Price was: %d\n", gasPrice)
 
-	//The docs suggested this but its deprecated now
-	//tx := types.NewTransaction(nonce, toAddress, transferAmount, gasLimit, gasPrice, nil)
-
-	//make and sign the transaction the new way
-	transaction := types.NewTx(&types.DynamicFeeTx{
-		ChainID:   chainID,
-		Nonce:     nonce,
-		GasFeeCap: big.NewInt(1).Mul(big.NewInt(21000), big.NewInt(1000000000)),
-		Gas:       uint64(21000),
-		To:        &destinationAccount.Address,
-		Value:     transferAmount,
-		Data:      nil,
-	})
-
-	//signedTx, err := myKeyStore.SignTx(sourceAccount, transaction, chainID)
-	//panicIfError(err)
-
 	sourcePrivateKey, err := crypto.HexToECDSA(sourcePrivateKeyString)
 	if err != nil {
 		log.Fatal(err)
 	}
-	signedTx, err := types.SignTx(transaction, types.NewEIP155Signer(chainID), sourcePrivateKey)
+
+	//transfer into the new account
+	gasLimit := uint64(21000)
+
+	loadTransaction := types.NewTransaction(nonce, destinationAccount.Address, transferAmount, gasLimit, gasPrice, nil)
+
+	signedTx, err := types.SignTx(loadTransaction, types.NewEIP155Signer(chainID), sourcePrivateKey)
 	panicIfError(err)
 
 	err = client.SendTransaction(context.Background(), signedTx)
 	panicIfError(err)
 
-	fmt.Printf("SourceAddress\n")
+	fmt.Printf("==Source Address==\n")
 	logBalancesForAddress(client, sourceAddress)
-	fmt.Printf("destinationAddress\n")
+
+	fmt.Printf("==Destination Address==\n")
 	logBalancesForAddress(client, destinationAccount.Address)
 }
 
@@ -87,28 +78,22 @@ func panicIfError(err error) {
 func logBalancesForAddress(client *ethclient.Client, toAddress common.Address) {
 	balance := getBalance(client, toAddress)
 	pendingBalance, err := client.PendingBalanceAt(context.Background(), toAddress)
-	if err != nil {
-		log.Fatal(err)
-	}
+	panicIfError(err)
 
 	fmt.Printf("Account balance for %s was %d wei or %f ether\n", toAddress, balance, weiToEther(balance))
-	fmt.Printf("pendingBalance: %d\n", pendingBalance)
+	fmt.Printf("pendingBalance: %d\n\n", pendingBalance)
 }
 
 func getNonceForWalletAddress(client *ethclient.Client, sourceAddress common.Address) uint64 {
 	nonce, err := client.PendingNonceAt(context.Background(), sourceAddress)
-	if err != nil {
-		log.Fatal(err)
-	}
+	panicIfError(err)
 
 	return nonce
 }
 
 func getBalance(client *ethclient.Client, address common.Address) *big.Int {
 	balance, err := client.BalanceAt(context.Background(), address, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	panicIfError(err)
 
 	return balance
 }
@@ -116,6 +101,7 @@ func getBalance(client *ethclient.Client, address common.Address) *big.Int {
 func weiToEther(balance *big.Int) *big.Float {
 	fbalance := new(big.Float)
 	fbalance.SetString(balance.String())
+
 	return new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
 }
 
